18e9e1c2ddd5c49f541071fdbbc1bf3af7dfef54
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-24 13:29:25 -0400

    commenting out db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-const mongoURL = process.env.MONGO_URL;
-mongoose.connect(mongoURL,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(res => {
-        console.log("Connected to db");
-    }).catch(err => {
-        console.log("error connecting db");
-    })
+// const mongoose = require("mongoose");
+// const mongoURL = process.env.MONGO_URL;
+// mongoose
+//   .connect(mongoURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((res) => {
+//     console.log("Connected to db");
+//   })
+//   .catch((err) => {
+//     console.log("error connecting db");
+//   });
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useCreateIndex", true);
+// mongoose.set("useUnifiedTopology", true);
 
-module.exports = mongoose
+// module.exports = mongoose;
